7eb69d798a66908f1489c7f8d6a7d01abf8c831e
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-08-16 19:34:07 +0900

    install-devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # resources :pages, only: [:index]
-  get 'pages/index'
-
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'pages#index'
+  get 'pages/index'
 end
