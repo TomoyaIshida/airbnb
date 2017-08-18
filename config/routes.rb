90e37b5436dd9392821fae252f1ada8335b95213
TomoyaIshida <ishidatomoya@ishidatomoya-no-MacBook-Pro.local>
2017-08-18 19:25:11 +0900

    edit-mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'pages#index'
+  root :to => 'pages#index'
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
